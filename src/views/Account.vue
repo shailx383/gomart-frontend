9f464897a60157b48dc15fc9513b53dc902bb110
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-11-30 00:57:41 +0530

    corrected modals in account page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -45,7 +45,16 @@
             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">₹{{ewallet.amount}}</dd>
             <button @click = "showModal" type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" data-modal-toggle="wallet-modal" >Add money</button>
             <div v-if="modal_shown" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+              <div class="relative w-full max-w-md h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button @click = "showModal" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="wallet-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
               <Wallet></Wallet>
+    </div>
+    </div>
     </div>
     <div v-if="modal_shown" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
           </div>
