4b570b421697c11fe1d55143bdadf9d1b4bfb8a2
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-12-02 14:41:19 +0530

    added more popups

---
+++ src/components/CheckoutPopUp.vue
@@ -0,0 +1,28 @@
+<template>
+  
+    <div class="py-6 px-6 lg:px-8">
+        <h2 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Success!</h2>
+            <div>
+                <h3>Your order has been placed.</h3>
+            </div>
+            <br/><br/>
+            <button @click="goToMain" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Home Page</button>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+name: 'CheckoutPopUp',
+data(){
+return{
+}
+},
+methods: {
+goToMain(){
+    this.$router.push('/main')
+}
+}
+}
+</script>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -81,6 +81,19 @@
     </div>
     </div>
     </div>
+    <div v-if="boughtShown" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+              <div class="relative w-full max-w-md h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button @click = "checkedOut()" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="wallet-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+              <CheckoutPopUp></CheckoutPopUp>
+    </div>
+    </div>
+    </div>
+    <div v-if="boughtShown" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
 <Footer></Footer>
 </template>
 
@@ -88,6 +101,7 @@
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 import WalletError from '@/components/WalletError.vue'
+import CheckoutPopUp from '@/components/CheckoutPopUp.vue'
 export default {
     name: 'Checkout',
     data(){
@@ -95,10 +109,12 @@ export default {
             products: [],
             totalPrice: 0,
             totalQuan: 0,
-            error_shown: false
+            error_shown: false,
+            error: '',
+            boughtShown: false
         }
     },
-    components: {Navbar, Footer, WalletError},
+    components: {Navbar, Footer, WalletError, CheckoutPopUp},
     methods: {
         total_price(){
         for (let i = 0; i < this.products.length; i++){
@@ -115,11 +131,21 @@ export default {
       },
       placeOrder(){
         axios.post('https://gomart-production.up.railway.app/user/cart/checkout', {userId: localStorage.logged})
-            .catch((error) => this.showError())
+        .then(response => {
+                            this.checkedOut();
+                            }) 
+        .catch(error => {this.error = error.response.data.message;
+                          if (this.error =='Insufficient balance'){
+                            this.showError()
+                          }
+        })
 
-            
-        
-        
+      },
+      goToMain(){
+        this.$router.push('/main')
+      },
+      checkedOut(){
+        this.boughtShown = !this.boughtShown
       }
     },
     mounted(){
