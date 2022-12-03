57f0a33926579e978fa7d3c96c10b7dd8d452f45
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-12-04 02:16:47 +0530

    fixed more navs

--- src/views/AddProd_new.vue
+++ src/views/AddProd_new.vue
@@ -41,9 +41,9 @@
                <MenuItem v-slot="{ active }">
                  <a href="/account" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
                </MenuItem>
-               <!-- <MenuItem v-slot="{ active }">
-                 <a href="/pastorders" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Past Orders</a>
-               </MenuItem> -->
+               <MenuItem v-slot="{ active }">
+                 <a href="/rstpass" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Change Password</a>
+               </MenuItem>
                <MenuItem v-slot="{ active }">
                  <a @click= "logoutUser" href="/" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
                </MenuItem>
