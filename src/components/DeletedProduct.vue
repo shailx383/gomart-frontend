fbe7fc85b69f02f4df48e67655283c00b1a23cd5
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-12-04 03:24:54 +0530

    re routing after add and edit

--- src/components/DeletedProduct.vue
+++ src/components/DeletedProduct.vue
@@ -6,7 +6,7 @@
                 <h3>Your product has been removed from the store.</h3>
             </div>
             <br/><br/>
-            <!-- <button @click="goToMain" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Account</button> -->
+            <button @click="goToMain" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go to Dashboard</button>
         
     </div>
 </template>
@@ -21,9 +21,19 @@ return{
 }
 },
 methods: {
-goToMain(){
-    this.$router.push('/')
+    goToMain(){
+    if (this.role == 'MANAGER'){
+        this.$router.push('/mainman')
+    }
+    else if (this.role == 'ADMIN'){
+        this.$router.push('/mainadm')
+    }
+    
 }
+},
+mounted(){
+    axios.get('https://gomart-production.up.railway.app/user/'+localStorage.logged)
+        .then(response => {this.role = response.data.role})
 }
 }
 </script>
