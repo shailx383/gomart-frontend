54c8eecf9ad38ad09ba6d81d36046333c94a57de
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-12-05 18:09:23 +0530

    added wrong password pop up

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -78,6 +78,20 @@
     </div>
     </div>
     <div v-if="showForgot" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+
+    <div v-if="wrong" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+              <div class="relative w-full max-w-md h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button @click = "wrongPassword()" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="wallet-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+              <PasswordError></PasswordError>
+    </div>
+    </div>
+    </div>
+    <div v-if="wrong" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
   </t-card>
 </template>
 
@@ -85,9 +99,10 @@
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 import LoginError from '../components/LoginError.vue'
 import ForgotPass from '../components/ForgotPass.vue'
+import PasswordError from '../components/PasswordError.vue'
 export default {
   name: 'Login',
-  components: {LoginError,ForgotPass},
+  components: {LoginError,ForgotPass, PasswordError},
   data(){
     return{
       logged: '',
@@ -99,6 +114,7 @@ export default {
       error: '',
       showError: false,
       showForgot: false,
+      wrong: false
     }
   },
   methods: {
@@ -112,6 +128,9 @@ export default {
                           this.error = error.response.data.message;
                           if (this.error =='User does not exist'){
                             this.userError()
+                          }
+                          else if (this.error == 'Invalid Password'){
+                            this.wrongPassword()
                           }
         })
     },
@@ -131,6 +150,9 @@ export default {
     },
     userForgot(){
       this.showForgot = !this.showForgot
+    },
+    wrongPassword(){
+      this.wrong = !this.wrong
     }
   },
   mounted(){
