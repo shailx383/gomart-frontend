038141f67b262fded799cbe3153fefb3f1616def
shailx383 <65397549+shailx383@users.noreply.github.com>
2022-12-05 18:59:18 +0530

    added length of response

--- src/views/ProductSearch.vue
+++ src/views/ProductSearch.vue
@@ -5,7 +5,7 @@
 			class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
 		>
 			<h2 class="text-2xl font-bold tracking-tight text-gray-900">
-				Results for {{ searchName }}:
+				Results for {{ searchName }} ({{product.length}}):
 			</h2>
 
 			<div
