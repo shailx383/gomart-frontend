0de5b844f90efcbc76478820275b0ae37feb365e
majimearun <arunachala0619@gmail.com>
2022-12-06 17:53:52 +0530

    removed uselss input in initwallet.vue

--- src/components/InitWallet.vue
+++ src/components/InitWallet.vue
@@ -30,23 +30,6 @@
 					Add ₹1000 to wallet to proceed:
 				</h3>
 				<form @submit="topUpWallet" class="space-y-6" action="#">
-					<div>
-						<label
-							for="amt"
-							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-							>Enter amount:</label
-						>
-						<input
-							readonly
-							v-model="edit_info.amount"
-							type="number"
-							name="amt"
-							id="amt"
-							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-							default="1000"
-							required
-						/>
-					</div>
 					<button
 						type="submit"
 						class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
