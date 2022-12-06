ed127176a28f85e4f60b99e0258e5293226f88a7
majimearun <arunachala0619@gmail.com>
2022-12-06 10:11:07 +0530

    added better carousel images

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="carousel w-full max-h-15">
 		<div id="slide1" class="carousel-item relative w-full">
-			<img src="../assets/banner_12-1294x372.jpg" class="w-full" />
+			<img src="../assets/c1.jpg" class="w-full" />
 			<div
 				class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
 			>
@@ -11,7 +11,7 @@
 		</div>
 		<div id="slide2" class="carousel-item relative w-full">
 			<img
-				src="../assets/orchard-product-fruit-banner-grocery-supermarket-store-shelves-variety-different-orgnaic-fresh-ripe-pears-apples-138064305.jpg"
+				src="../assets/c2.jpg"
 				class="w-full"
 			/>
 			<div
@@ -22,11 +22,38 @@
 			</div>
 		</div>
 		<div id="slide3" class="carousel-item relative w-full">
-			<img src="../assets/supermarket-banner.jpg" class="w-full" />
+			<img src="../assets/c3.jpg" class="w-full" />
 			<div
 				class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
 			>
 				<a href="#slide2" class="btn btn-circle">❮</a>
+				<a href="#slide4" class="btn btn-circle">❯</a>
+			</div>
+		</div>
+		<div id="slide4" class="carousel-item relative w-full">
+			<img src="../assets/c4.jpg" class="w-full" />
+			<div
+				class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+			>
+				<a href="#slide3" class="btn btn-circle">❮</a>
+				<a href="#slide5" class="btn btn-circle">❯</a>
+			</div>
+		</div>
+		<div id="slide5" class="carousel-item relative w-full">
+			<img src="../assets/c5.jpg" class="w-full" />
+			<div
+				class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+			>
+				<a href="#slide4" class="btn btn-circle">❮</a>
+				<a href="#slide6" class="btn btn-circle">❯</a>
+			</div>
+		</div>
+		<div id="slide6" class="carousel-item relative w-full">
+			<img src="../assets/c6.jpg" class="w-full" />
+			<div
+				class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+			>
+				<a href="#slide5" class="btn btn-circle">❮</a>
 				<a href="#slide1" class="btn btn-circle">❯</a>
 			</div>
 		</div>
