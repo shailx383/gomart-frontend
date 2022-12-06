ed6137ceaa4d64bd0616d5b18aaa4fa210ac97e1
majimearun <arunachala0619@gmail.com>
2022-12-06 14:25:25 +0530

    phoneNumber -> phone

--- src/components/UpdateUser.vue
+++ src/components/UpdateUser.vue
@@ -53,7 +53,7 @@
 					>Phone number:</label
 				>
 				<input
-					v-model="edit_info.phoneNumber"
+					v-model="edit_info.phone"
 					type="tel"
 					name="phone"
 					id="phone"
@@ -98,7 +98,7 @@ export default {
 				lastName: "",
 				dob: "2003-08-03",
 				email: "",
-				phoneNumber: "",
+				phone: "",
 				address: "",
 			},
 		};
